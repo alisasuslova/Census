752ad7a42e9ab5ccd6295769bb588d7afe7bcc57
alisasuslova <alisasuslova1408@gmail.com>
2022-01-16 20:45:15 +0300

    16.02.22 streams fixed

--- src/Main.java
+++ src/Main.java
@@ -16,32 +16,33 @@ public class Main {
                     Education.values()[new Random().nextInt(Education.values().length)])
             );
         }
-        System.out.println(persons); //потом убрать
 
         List<Person> list = (List)persons;
         long count = list.stream()
                 .filter(a -> a.getAge() < 18)
                 .count();
-        System.out.println("Количество несовершеннолетних : " + count);
+        System.out.println("Количество несовершеннолетних : " + count + "\n");
 
 
-        List<Person> list2 = (List)persons;
-        Stream<Person> stream_1 = list2.stream();
-        Stream<Person> stream_2 = stream_1.filter(s -> s.getSex() == Sex.MAN);
-        Stream<Person> stream_3 = stream_2.filter(a -> a.getAge() >= 18);
-        Stream<Person> stream_4 = stream_3.filter(a -> a.getAge() < 27);
-        List<String> stream_5 = stream_4.map(f -> f.getFamily()).collect(Collectors.toList());
-        System.out.println("Cписок призывников: \n" + stream_5);
+        List<Person> list1 = (List)persons;
+        List<String> list2 = list1.stream()
+                .filter(s -> s.getSex() == Sex.MAN)
+                .filter(a -> a.getAge() >= 18)
+                .filter(a -> a.getAge() < 27)
+                .map(f -> f.getFamily())
+                .collect(Collectors.toList());
+        System.out.println("Cписок призывников: \n" + list2 + "\n");
 
 
         List<Person> list3 = (List)persons;
-        Stream<Person> stream_6 = list3.stream();
-        Stream<Person> stream_7 = stream_6.filter(a -> a.getAge() >= 18);
-        Stream<Person> stream_8 = stream_7.filter(a -> a.getAge() < 65);
-        Stream<Person> stream_9 = stream_8.filter(e -> e.getEducation() == Education.HIGHER);
-        Stream<Person> stream_10 = stream_9.sorted((f1, f2) -> f1.getFamily().compareToIgnoreCase(f2.getFamily()));
-        List<Person> stream_11 = stream_10.collect(Collectors.toList());
-        System.out.println("Cписок потенциально работоспособных людей с высшим образованием: \n" + stream_11);
+        list3.stream()
+                .filter(a -> a.getAge() >= 18)
+                .filter(a -> a.getAge() < 65)
+                .filter(e -> e.getEducation() == Education.HIGHER)
+                .sorted((f1, f2) -> f1.getFamily().compareToIgnoreCase(f2.getFamily()))
+                .collect(Collectors.toList());
+        System.out.println("Cписок потенциально работоспособных людей с высшим образованием: \n" + list3);
 
     }
 }
+
